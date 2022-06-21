03b664819380998da32cd2e4953958f3d42bd223
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-21 21:42:23 +0000

    started project, added nav, header, avatar,name, and input box. Created related css and js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  console.log('composer loaded')
+
+  $("#tweet-text").on('input', function () {
+    //$(this).val().length);
+    //$(this).next().children().next().val()
+
+    if ($(this).val().length > 140) {
+      $($(this).next().children().next()).addClass ('over-count')
+      $(this).next().children().next().val(140 - $(this).val().length)
+    } else {
+       $(this).next().children().next().val(140 - $(this).val().length)
+    }
+   
+
+
+  });
+
+});
