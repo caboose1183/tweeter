b797f3fddf69cc9ad2d03ef66e88c05d5ef7cc36
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-22 21:12:24 +0000

    added locaTweets function, ajax GET request to server for array of tweets to display tweets on index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,40 +5,12 @@
  */
 
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-
 function createTweetElement(tweetData) {
   const name = tweetData.user.name;
   const avatar = tweetData.user.avatars;
   const handle = tweetData.user.handle;
   const content = tweetData.content.text;
-  const date = tweetData.created_at;
+  const date = timeago.format (tweetData.created_at);
 
   let $tweet = ` <article class="tweeter-article" >
   <header class="tweeter-header">
@@ -86,14 +58,22 @@ $('.tweet-form').submit(function (event) {
 
   let $data = $(this).serialize()
 
-  console.log ($data)
   $.ajax ({
     type: 'POST',
     url: '/tweets',
     data: $data,
-  
-
-
-
   })
 })
+
+function localTweets () {
+  let $data = $(this).serialize()
+
+  $.ajax ({
+    type: 'GET',
+    url: '/tweets',
+    data: $data,
+    success: response => {
+      renderTweets(response)
+    }
+  })
+}
